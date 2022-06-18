312841a28b2d61d41ad277d33efac6fc9f584773
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-06-18 17:28:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// Require express and body-parser
+
 const express = require("express")
 const bodyParser = require("body-parser")
-// Initialize express and define a port
+
 const app = express()
 const PORT = 3000
-// Tell express to use body-parser's JSON parsing
-//...
+
 app.use(bodyParser.json())
 app.post("/hook", (req, res) => {
-  console.log(req.body) // Call your action on the request here
-  res.status(200).end() // Responding is important
-})
-//...
-// Start express on the defined port
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+  console.log(req.body)
+  res.status(200).end() 
+
+app.listen(PORT, () => console.log(`🚀 Server is  running on port ${PORT}`))
